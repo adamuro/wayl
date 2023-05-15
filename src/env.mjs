c1f0bf47ad98f8161564b79f862d01921f0fc962
adamuro <horosporo@gmail.com>
2023-05-15 19:55:33 +0200

    Add initial db schema, use vercel psql

--- src/env.mjs
+++ src/env.mjs
@@ -3,7 +3,13 @@ import { createEnv } from '@t3-oss/env-nextjs';
 
 export const env = createEnv({
   server: {
-    DATABASE_URL: z.string().url(),
+    PSQL_URL: z.string(),
+    PSQL_PRISMA_URL: z.string(),
+    PSQL_URL_NON_POOLING: z.string(),
+    PSQL_USER: z.string(),
+    PSQL_HOST: z.string(),
+    PSQL_PASSWORD: z.string(),
+    PSQL_DATABASE: z.string(),
     CLERK_SECRET_KEY: z.string(),
     NODE_ENV: z.string().regex(/production|development|test/),
   },
@@ -12,7 +18,13 @@ export const env = createEnv({
   },
   runtimeEnv: {
     NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
-    DATABASE_URL: process.env.DATABASE_URL,
+    PSQL_URL: process.env.PSQL_URL,
+    PSQL_PRISMA_URL: process.env.PSQL_PRISMA_URL,
+    PSQL_URL_NON_POOLING: process.env.PSQL_URL_NON_POOLING,
+    PSQL_USER: process.env.PSQL_USER,
+    PSQL_HOST: process.env.PSQL_HOST,
+    PSQL_PASSWORD: process.env.PSQL_PASSWORD,
+    PSQL_DATABASE: process.env.PSQL_DATABASE,
     CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
     NODE_ENV: process.env.NODE_ENV,
   },
