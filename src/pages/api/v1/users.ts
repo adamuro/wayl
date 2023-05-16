6bf3ab581b1a49ce047194904c3e501c617c53f6
Adam Turowski <horosporo@gmail.com>
2023-05-16 19:18:18 +0200

    Add create user webhook

--- src/pages/api/v1/users.ts
+++ src/pages/api/v1/users.ts
@@ -1,32 +1,54 @@
 import type { UserWebhookEvent } from '@clerk/clerk-sdk-node';
+import type { IncomingHttpHeaders } from 'http2';
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { Webhook } from 'svix';
+import { Webhook, WebhookVerificationError } from 'svix';
 import { env } from '~/env.mjs';
+import { prisma } from '~/server/db';
+
 const webhookSecret: string = env.CLERK_WEBHOOK_SECRET_KEY;
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== 'POST') return res.status(405).end();
+
   let event: UserWebhookEvent | null = null;
 
   const wh = new Webhook(webhookSecret);
-  const payload = (await req.body) as UserWebhookEvent;
-  const payloadString = JSON.stringify(payload);
-  const headerPayload = req.headers;
-  const svixHeaders = {
-    'svix-id': headerPayload['svix-id'] as string,
-    'svix-timestamp': headerPayload['svix-timestamp'] as string,
-    'svix-signature': headerPayload['svix-signature'] as string,
-  };
+  const payloadString = JSON.stringify(req.body);
+  const svixHeaders = getSvixHeaders(req.headers);
 
   try {
     event = wh.verify(payloadString, svixHeaders) as UserWebhookEvent;
   } catch (error) {
-    console.log(error);
-    return res.status(400).send('Error occured');
+    if (error instanceof WebhookVerificationError) return res.status(400).send(error.message);
+    return res.status(400).send('Unexpected webhook verification error.');
   }
 
-  if (event.type === 'user.created') {
-    console.log(`User ${event.data.id} created`);
+  switch (event.type) {
+    case 'user.created':
+    case 'user.updated':
+      const { id, profile_image_url, username, first_name } = event.data;
+      void prisma.user.upsert({
+        where: { clerkId: id },
+        update: { imageUrl: profile_image_url },
+        create: {
+          clerkId: id,
+          imageUrl: profile_image_url,
+          name: username || first_name,
+        },
+      });
+      return res.status(201).end();
+    case 'user.deleted':
+      void prisma.user.delete({ where: { clerkId: event.data.id } });
+      return res.status(200).end();
+    default:
+      return res.status(400).send('Unsupported event type.');
   }
+}
 
-  res.status(201).send('Success');
+function getSvixHeaders(headers: IncomingHttpHeaders) {
+  return {
+    'svix-id': headers['svix-id'] as string,
+    'svix-timestamp': headers['svix-timestamp'] as string,
+    'svix-signature': headers['svix-signature'] as string,
+  };
 }
