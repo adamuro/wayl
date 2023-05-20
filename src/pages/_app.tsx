d3a62754efa641c89a15f1526e24c73f9b9a46da
Adam Turowski <horosporo@gmail.com>
2023-05-20 15:21:48 +0200

    Organize imports

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,8 @@
-import { type AppType } from 'next/app';
-
-import { api } from '~/utils/api';
-
-import '~/styles/globals.css';
 import { ClerkProvider } from '@clerk/nextjs';
 import { dark } from '@clerk/themes';
+import { type AppType } from 'next/app';
+import '~/styles/globals.css';
+import { api } from '~/utils/api';
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
