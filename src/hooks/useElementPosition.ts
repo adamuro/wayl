1c4da5e8f93eb8759a99dfcd304079761c0bffd5
Adam Turowski <horosporo@gmail.com>
2023-05-22 23:09:09 +0200

    Fix user button position bug

--- src/hooks/useElementPosition.ts
+++
@@ -1,9 +0,0 @@
-import type { RefObject } from 'react';
-
-export const useElementPosition = (ref: RefObject<HTMLLIElement>) => {
-  const rect = ref.current?.getBoundingClientRect();
-  return {
-    x: rect?.x || 0,
-    y: rect?.y || 0,
-  };
-};
