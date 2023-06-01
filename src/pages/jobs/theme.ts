07a0b875a655f487a7c05ce397d93e705b6270dd
adamuro <horosporo@gmail.com>
2023-06-01 15:23:27 +0200

    Set one cron job per day (free plan limit)

--- src/pages/jobs/theme.ts
+++ src/pages/jobs/theme.ts
@@ -1,3 +1,7 @@
-export default function theme() {
-  console.log('theme');
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { prisma } from '~/server/db';
+
+export default async function theme(req: NextApiRequest, res: NextApiResponse) {
+  await prisma.theme.update({ where: { id: 1 }, data: { date: new Date() } });
+  res.status(200).end();
 }
