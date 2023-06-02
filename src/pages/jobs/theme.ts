e497aa617875676eb99427108d1e26f9d4bae933
Adam Turowski <horosporo@gmail.com>
2023-06-02 14:48:15 +0200

    Move cron jobs to api folder

--- src/pages/jobs/theme.ts
+++
@@ -1,7 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-import { prisma } from '~/server/db';
-
-export default async function theme(req: NextApiRequest, res: NextApiResponse) {
-  await prisma.theme.update({ where: { id: 1 }, data: { date: new Date() } });
-  res.status(200).end();
-}
