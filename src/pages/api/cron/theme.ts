ad5acbc32c7fbdc96a1a7f79ae78d855940f50b7
Adam Turowski <horosporo@gmail.com>
2023-06-02 14:54:04 +0200

    Add test database operation to cron job

--- src/pages/api/cron/theme.ts
+++ src/pages/api/cron/theme.ts
@@ -1,5 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
+import { prisma } from '~/server/db';
 
-export default function theme(req: NextApiRequest, res: NextApiResponse) {
+export default async function theme(req: NextApiRequest, res: NextApiResponse) {
+  await prisma.theme.update({ where: { id: 1 }, data: { date: new Date() } });
   res.status(200).json({ message: 'success' });
 }
