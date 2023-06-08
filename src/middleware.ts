1cb26f42ae46936e3d4c81b613529c7f4ccee504
Adam Turowski <horosporo@gmail.com>
2023-06-08 18:13:17 +0200

    Add webhooks route to public routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from '@clerk/nextjs';
 
 export default authMiddleware({
-  publicRoutes: ['/api/v1/cron/:job'],
+  publicRoutes: ['/api/v1/cron/:job', '/api/v1/wh/:hook'],
 });
 
 export const config = {
