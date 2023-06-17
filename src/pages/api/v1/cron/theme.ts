db5779ac8b3859a3414be83b8dd97583b24780f5
Adam Turowski <horosporo@gmail.com>
2023-06-17 15:35:12 +0200

    Implement theme switch cron job

--- src/pages/api/v1/cron/theme.ts
+++ src/pages/api/v1/cron/theme.ts
@@ -2,9 +2,32 @@ import { verifySignature } from '@upstash/qstash/nextjs';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { prisma } from '~/server/db';
 
+async function getRandomAvailableTheme() {
+  const availableThemes = await prisma.theme.findMany({ where: { available: true } });
+  return availableThemes.at(Math.floor(Math.random() * availableThemes.length));
+}
+
+async function getOldestTheme() {
+  const oldestThemes = await prisma.theme.findMany({ orderBy: { date: 'asc' }, take: 1 });
+  return oldestThemes.at(0);
+}
+
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  await prisma.theme.update({ where: { id: 1 }, data: { date: new Date() } });
-  res.status(200).end();
+  if (req.method !== 'POST') return res.status(405).end();
+
+  const id = (await getRandomAvailableTheme())?.id || (await getOldestTheme())?.id;
+  if (!id) return res.status(500).json({ error: 'No themes in database.' });
+
+  const theme = await prisma.theme.update({
+    where: { id },
+    data: {
+      date: new Date(),
+      active: true,
+      available: false,
+    },
+  });
+
+  res.status(200).json(theme);
 }
 
 export default verifySignature(handler);
