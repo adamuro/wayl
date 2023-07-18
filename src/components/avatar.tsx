a75fcc1ec6bf1f73b5e2133b24f5f454856db2d0
Adam Turowski <horosporo@gmail.com>
2023-07-18 17:53:05 +0200

    Add default avatar image

--- src/components/avatar.tsx
+++ src/components/avatar.tsx
@@ -1,4 +1,6 @@
 import Image from 'next/image';
+import { useState } from 'react';
+import avatarDefault from '~/assets/avatar-default.png';
 
 interface AvatarProps {
   name: string;
@@ -6,14 +8,17 @@ interface AvatarProps {
 }
 
 export const Avatar = (props: AvatarProps) => {
+  const [error, setError] = useState(false);
+
   return (
     <div>
       <div className="w-10">
         <Image
           alt={`${props.name} profile picture`}
-          src={props.url}
+          src={error ? avatarDefault : props.url}
           width={40}
           height={40}
+          onError={() => setError(true)}
           className="h-10 w-10 rounded-full"
         />
       </div>
